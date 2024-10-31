49dc4a3b416d4f432ba32134569f02d0fd682f4a
Wallyuz <Wall.aquinoo@gmail.com>
2024-10-31 15:05:25 -0300

    Teste

--- src/Components/FilterGroup/FilterGroup.jsx
+++ src/Components/FilterGroup/FilterGroup.jsx
@@ -1,48 +1,32 @@
-//function que imprime o filter, atraves de const usando props
 import React from "react";
 
-const marcas =['Adidas','Balenciaga','K-Swiss','Nike','Puma'];
-const categoria = ['Estporte e lazer','Casual','Utilitário','Corrida'];
-const genero = ['Masculino','Feminino','Unisex'];
-
-<FilterGroup
-    title="Filtar por"
-    marcasProps={marcas}
-    categoriaProps={categoria}
-    generoProps={genero}
-/>
-
-function FilterGroup(props){
-    return(
+function FilterGroup(props) {
+    return (
         <div>
             <h2>{props.title}</h2>
 
             <h2>Marca</h2>
             <ul>
-                {props.marcasProps.map((item,index) => (
+                {props.marcasProps.map((item, index) => (
                     <li key={index}>{item}</li>
                 ))}
             </ul>
 
-
             <h2>Categoria</h2>
             <ul>
-                {props.categoriaProps.map((item,index) =>(
+                {props.categoriaProps.map((item, index) => (
                     <li key={index}>{item}</li>
-            ))}
+                ))}
             </ul>
-
 
             <h2>Gênero</h2>
             <ul>
-                {props.generoProps.map((item,index) => (
+                {props.generoProps.map((item, index) => (
                     <li key={index}>{item}</li>
-            ))}
+                ))}
             </ul>
         </div>
-
     );
-
 }
 
 export default FilterGroup;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import FilterGroup from './Components/FilterGroup/FilterGroup.jsx';
+
+const marcas = ['Adidas', 'Balenciaga', 'K-Swiss', 'Nike', 'Puma'];
+const categoria = ['Esporte e lazer', 'Casual', 'Utilitário', 'Corrida'];
+const genero = ['Masculino', 'Feminino', 'Unisex'];
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-  // npm i
-)
+    <FilterGroup
+      title="Filtrar por"
+      marcasProps={marcas}
+      categoriaProps={categoria}
+      generoProps={genero}
+    />
+  </StrictMode>
+);
