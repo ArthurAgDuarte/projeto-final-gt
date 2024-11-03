8548c46b7961c902ff5b5b6c43836064c5b107fc
Wallyuz <wall.aquinoo@gmail.com>
2024-11-03 10:45:45 -0300

    Funcionamento do card

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,15 +1,16 @@
-import "./ProductCard.css";
+import React from "react";
+import "./ProductCard.css"
 import { useNavigate } from "react-router-dom";
 import propTypes from "prop-types";
 
 export default function ProductCard({ produto }) {
-  const navigate =
-    useNavigate(); /*redireciona o usuário para a página produto*/
+  const navigate = useNavigate();
   const handleClick = () => navigate("/produto");
+
   return (
     <div className="product-card-container" onClick={handleClick}>
       <figure className="figure-container">
-        <span className="product-discont">30% OFF</span>
+        <span className="product-discount">30% OFF</span>
         <img
           className="product-image"
           src={produto.image}
@@ -17,8 +18,7 @@ export default function ProductCard({ produto }) {
         />
       </figure>
       <section className="product-container-informations">
-        <h2>{produto.nome}</h2>
-        <h1>K-Swiss V8 - Masculino</h1>
+        <h1>{produto.nome}</h1>
         <p className="product-container-information-paragraph">
           <span>${produto.price}</span>
           <span>${produto.priceDiscont}</span>
@@ -27,6 +27,12 @@ export default function ProductCard({ produto }) {
     </div>
   );
 }
+
 ProductCard.propTypes = {
-  produto: propTypes.object,
+  produto: propTypes.shape({
+    image: propTypes.string.isRequired,
+    nome: propTypes.string.isRequired,
+    price: propTypes.number.isRequired,
+    priceDiscont: propTypes.number.isRequired,
+  }).isRequired,
 };
