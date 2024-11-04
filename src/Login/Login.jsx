67ccc5a13731d6e630c55ae84f324032138d0c53
john6257 <voltoja35@gmail.com>
2024-11-04 15:00:11 -0300

    projeto final do Login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
 import './LoginPage.css'; // Adicione um arquivo CSS separado para estilização
-import google from '../assets/google.png';
-import facebook from '../assets/facebook.png'
 import img from '../assets/img.png'
-import tenis1 from '../assets/ténis1.png'
-import tenis2 from '../assets/ténis2.png'
-
+import tenis1 from '../assets/ténis1.png';
+import tenis2 from '../assets/ténis2.png';
+import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
   return (
     <div className="login-container">
 
-             <div className="tenis">
-                <img src={tenis1} alt="icon-img" /> 
-                <img src={tenis2} alt="icon-img" /> 
-             </div>
-
-
       <div className="login-card">
         <h2>Crie sua conta</h2>
         <p>
-          Já possui uma conta? <a href="/login">Entre aqui</a>.
+          Novo cliente? Então registre-se <Link href="/login">a aqui</Link>.
         </p>
         <form>
-          <label htmlFor="email">Email *</label>
+          <label htmlFor="email">Login *</label>
           <input
             type="email"
             id="email"
             placeholder="Insira seu email"
             required
-          />
+          />  
+
+        <label htmlFor="senha">Senha *</label>
+          <input
+            type="senha"
+            id="senha"
+            placeholder="Insira sua senha"
+            required
+          /> 
+          
           <button type="submit" className="create-account-button">
             Criar Conta
           </button>
@@ -40,7 +41,16 @@ export default function LoginPage() {
            </button>
 
         </div>
+
+<div className="container2">
+  
+          <div className="tenis">
+                 <img src={tenis1} alt="icon-img" />
+                  <div className="tenis2"><img src={tenis2} alt="icon-img" /> </div>
+               </div>
+</div>
       </div>
+    
     
   );
 }
