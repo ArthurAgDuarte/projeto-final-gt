2c02d592771afe64cb8dad01225881aec24956b9
Wallyuz <wall.aquinoo@gmail.com>
2024-11-04 14:14:05 -0300

    mudanças

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import FilterGroup from './Components/FilterGroup/FilterGroup.jsx';
 
-const marcas = ['Adidas', 'Balenciaga', 'K-Swiss', 'Nike', 'Puma'];
+const marcas = [' Adidas', ' Balenciaga', ' K-Swiss', ' Nike', ' Puma'];
 const categoria = ['Esporte e lazer', 'Casual', 'Utilitário', 'Corrida'];
 const genero = ['Masculino', 'Feminino', 'Unisex'];
 
