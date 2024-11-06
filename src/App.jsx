2097831c25c0760f93335c06409be9c453593e65
Seu Nome <voltoja35@gmail.com>
2024-11-06 13:10:09 -0300

    primeira parte

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-// Importa o BrowserRouter e chama de Router (alias == apelido)
+
+
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./Routes/AppRoutes";
-
+import React from 'react';
+import FormCadastro from "./Components/AppFormulario/FormCadastro";
 
 export default function App() {
   return (
     <Router>
         <AppRoutes/>
+        <FormCadastro/>
     </Router>
   );
 }
