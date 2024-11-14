2e1664d8b2646475cb527921af4ac1bbf523f1b1
italocampo <italosouzacampos99@gmail.com>
2024-11-14 13:45:29 -0300

    Conclusão - Italo(Front)

--- src/Components/ProductOptions/ProductOptions.jsx
+++ src/Components/ProductOptions/ProductOptions.jsx
@@ -22,7 +22,29 @@ export function ProductOptions() {
   return (
     <section className="product-options-container">
       <h3>Tamanho</h3>
-      <div></div>
+      <div>
+        {options_tamanhos.map((optionTamanho, index) => (
+          <span
+            key={index}
+            className={index === tamanhoAtivo ? "active-tamanho" : ""}
+            onClick={() => selectTamanho(index)}
+          >
+            {optionTamanho}
+          </span>
+        ))}
+      </div>
+      <h3>Cores</h3>
+
+      <div>
+        {options_cores.map((optionCor, index) => (
+          <span
+            key={index}
+            className={index === corAtiva ? "active-cor" : ""}
+            style={{ backgroundColor: optionCor }}
+            onClick={() => selectCor(index)}
+          ></span>
+        ))}
+      </div>
     </section>
   );
 }
