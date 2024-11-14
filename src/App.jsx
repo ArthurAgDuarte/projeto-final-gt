b75ef29e7f9cdd247236aae1033a9754bab8e7d4
Wallyuz <Wallison.vieira@aluno.uece.br>
2024-11-14 16:06:37 -0300

    app-atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import React, { useState, useEffect } from "react";
-import ProductCard from "./Components/ProductCard/ProductCard"; 
-import { BrowserRouter } from "react-router-dom";
-import { BuyBox } from "./Components/BuyBox/Buybox";
-import ProductListing from "./Components/ProductListing/ProductListing";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./Routes/AppRoutes";
 
-function App() {
-  const [produto, setProduto] = useState(null); 
-  const urlAPI = "https://6721a5a698bbb4d93ca91b8c.mockapi.io/ApiTeste/teste";
-
-  
-  useEffect(() => {
-    fetch(urlAPI)
-      .then((resultado) => resultado.json())
-      .then((data) => {
-        console.log(data); 
-        setProduto(data[0]); 
-      })
-      .catch((erro) => {
-        console.error("Erro", erro);
-        alert("Não foi possível carregar os dados");
-      });
-      
-  }, []);
-
+export default function App() {
   return (
-    <BrowserRouter>
-      <ProductListing / >
-    </BrowserRouter>
+
+    <><Router>
+      <AppRoutes />
+    </Router></>
+    
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
-import React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import FilterGroup from './Components/FilterGroup/FilterGroup.jsx';
-import ProductListing from './Components/ProductListing/ProductListing.jsx';
-import ProductCard from './Components/ProductCard/ProductCard.jsx';
-import App from './App.jsx';
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-const marcas = [' Adidas', ' Balenciaga', ' K-Swiss', ' Nike', ' Puma'];
-const categoria = ['Esporte e lazer', 'Casual', 'Utilitário', 'Corrida'];
-const genero = ['Masculino', 'Feminino', 'Unisex'];
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>
