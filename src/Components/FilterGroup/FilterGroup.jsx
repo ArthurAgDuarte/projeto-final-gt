aad686ab7fd8f9d9ba2a0bfc5959a077046be14e
Wallyuz <wall.aquinoo@gmail.com>
2024-11-15 21:08:33 -0300

    aparição do filter

--- src/Components/FilterGroup/FilterGroup.jsx
+++ src/Components/FilterGroup/FilterGroup.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./style.css";
 
-function FilterGroup(props) {
+function FilterGroup() {
+    const marcas = ['Adidas', 'Balenciaga', 'K-Swiss', 'Nike', 'Puma'];
+    const categorias = ['Esporte e lazer', 'Casual', 'Utilitário', 'Corrida'];
+    const generos = ['Masculino', 'Feminino', 'Unisex'];
+
     return (
         <div className="filter">
-            <h1>{props.title}</h1>
+            <h1>Filtros</h1>
             <hr />
             <h2>Marca</h2>
             <ul>
-                {props.marcasProps.map((item, index) => (
+                {marcas.map((item, index) => (
                     <li key={index}>
                         <input type="checkbox" />{item}
                     </li>
@@ -16,7 +20,7 @@ function FilterGroup(props) {
             </ul>
             <h2>Categoria</h2>
             <ul>
-                {props.categoriaProps.map((item, index) => (
+                {categorias.map((item, index) => (
                     <li key={index}>
                         <input type="checkbox" />{item}
                     </li>
@@ -24,7 +28,7 @@ function FilterGroup(props) {
             </ul>
             <h2>Gênero</h2>
             <ul>
-                {props.generoProps.map((item, index) => (
+                {generos.map((item, index) => (
                     <li key={index}>
                         <input type="checkbox" />{item}
                     </li>
@@ -35,3 +39,42 @@ function FilterGroup(props) {
 }
 
 export default FilterGroup;
+
+
+// import React from "react";
+// import "./style.css";
+
+// function FilterGroup(props) {
+//     return (
+//         <div className="filter">
+//             <h1>{props.title}</h1>
+//             <hr />
+//             <h2>Marca</h2>
+//             <ul>
+//                 {props.marcasProps.map((item, index) => (
+//                     <li key={index}>
+//                         <input type="checkbox" />{item}
+//                     </li>
+//                 ))}
+//             </ul>
+//             <h2>Categoria</h2>
+//             <ul>
+//                 {props.categoriaProps.map((item, index) => (
+//                     <li key={index}>
+//                         <input type="checkbox" />{item}
+//                     </li>
+//                 ))}
+//             </ul>
+//             <h2>Gênero</h2>
+//             <ul>
+//                 {props.generoProps.map((item, index) => (
+//                     <li key={index}>
+//                         <input type="checkbox" />{item}
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
+
+// export default FilterGroup;
--- src/Pages/ProductLP.jsx
+++ src/Pages/ProductLP.jsx
@@ -1,9 +1,10 @@
 import ProductListing from "../Components/ProductListing/ProductListing";
-
+import FilterGroup from "../Components/FilterGroup/FilterGroup";
 export default function ProductLP() {
   return (
     <>
-      {/* <ProductListing limit= {12}/> */}
+      <FilterGroup />
+     {/* <ProductListing limit= {12}/>  */}
     </>
   );
 }
