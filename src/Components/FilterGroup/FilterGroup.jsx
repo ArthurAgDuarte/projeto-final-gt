bb4617f2e00a3adc49257a5bc4a9eb22efae99c9
Wallyuz <wall.aquinoo@gmail.com>
2024-11-17 19:04:19 -0300

    Highlights

--- src/Components/FilterGroup/FilterGroup.jsx
+++ src/Components/FilterGroup/FilterGroup.jsx
@@ -8,7 +8,7 @@ function FilterGroup() {
 
     return (
         <div className="filter">
-            <h1>Filtros</h1>
+            <h1>Filtar por</h1>
             <hr />
             <h2>Marca</h2>
             <ul>
@@ -41,40 +41,3 @@ function FilterGroup() {
 export default FilterGroup;
 
 
-// import React from "react";
-// import "./style.css";
-
-// function FilterGroup(props) {
-//     return (
-//         <div className="filter">
-//             <h1>{props.title}</h1>
-//             <hr />
-//             <h2>Marca</h2>
-//             <ul>
-//                 {props.marcasProps.map((item, index) => (
-//                     <li key={index}>
-//                         <input type="checkbox" />{item}
-//                     </li>
-//                 ))}
-//             </ul>
-//             <h2>Categoria</h2>
-//             <ul>
-//                 {props.categoriaProps.map((item, index) => (
-//                     <li key={index}>
-//                         <input type="checkbox" />{item}
-//                     </li>
-//                 ))}
-//             </ul>
-//             <h2>Gênero</h2>
-//             <ul>
-//                 {props.generoProps.map((item, index) => (
-//                     <li key={index}>
-//                         <input type="checkbox" />{item}
-//                     </li>
-//                 ))}
-//             </ul>
-//         </div>
-//     );
-// }
-
-// export default FilterGroup;
--- src/Pages/ProductLP.jsx
+++ src/Pages/ProductLP.jsx
@@ -1,10 +1,14 @@
 import ProductListing from "../Components/ProductListing/ProductListing";
 import FilterGroup from "../Components/FilterGroup/FilterGroup";
+import HighlightButtons from "../Components/highlightButtons/highlightButtons";
+
 export default function ProductLP() {
   return (
     <>
-      <FilterGroup />
-     {/* <ProductListing limit= {12}/>  */}
+      {/* <FilterGroup />
+     <ProductListing limit= {12}/>  */}
+     <HighlightButtons />
+     
     </>
   );
 }
