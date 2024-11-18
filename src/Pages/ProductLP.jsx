685b38b26bf6080ad44c94cf641506d0e4c9fd63
Wallyuz <Wallison.vieira@aluno.uece.br>
2024-11-18 12:56:05 -0300

    continuação productlistingPage

--- src/Pages/ProductLP.jsx
+++ src/Pages/ProductLP.jsx
@@ -2,14 +2,16 @@ import Header from '../Components/Header/Header.jsx';
 import Footer from '../Components/Footer/Footer.jsx';
 import ProductListing from "../Components/ProductListing/ProductListing";
 import FilterGroup from "../Components/FilterGroup/FilterGroup";
+import "./style.css"
 export default function ProductLP() {
   return (
     <>
       <Header/>
-      <main>
+      <main className='teste'>
+
       <FilterGroup />
       <ProductListing limit={12}/>  
-      </main> 
+      </main>
       <Footer/>
      </>
 );
