d27d6771271d9d67e999eaa92e454c46f93e09b8
Wallyuz <Wallison.vieira@aluno.uece.br>
2024-11-18 11:13:51 -0300

    Progresso vlp

--- src/Pages/ProductLP.jsx
+++ src/Pages/ProductLP.jsx
@@ -2,12 +2,14 @@ import Header from '../Components/Header/Header.jsx';
 import Footer from '../Components/Footer/Footer.jsx';
 import ProductListing from "../Components/ProductListing/ProductListing";
 import FilterGroup from "../Components/FilterGroup/FilterGroup";
-import HighlightButtons from "../Components/highlightButtons/highlightButtons";
 export default function ProductLP() {
   return (
     <>
       <Header/>
-      <HighlightButtons />   
+      <main>
+      <FilterGroup />
+      <ProductListing limit={12}/>  
+      </main> 
       <Footer/>
      </>
 );
