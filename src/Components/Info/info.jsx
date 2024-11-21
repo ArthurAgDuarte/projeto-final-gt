ee198ff18346b85b2208e5e696a8f52155febe5c
Wallyuz <wall.aquinoo@gmail.com>
2024-11-21 20:19:23 -0300

    Pagina de InfoUser

--- src/Components/Info/info.jsx
+++ src/Components/Info/info.jsx
@@ -17,17 +17,28 @@ export default function Info() {
           </ul>
         </section>
 
-        <section className="conteudo">
-         <h2 className="titulo">Minhas Informações</h2> 
-         <hr className="line" />
-        <div className="InfoUser">
-        <h1 className="Titulo">Informações Pessoais</h1>
-        
+        <section className="info-area">
+        <p className="Inicial">Minhas Infomações</p>
+        <hr className="divisor" />
+        <div className="personal-info">
+        <h1>Informações Pessoais</h1>
+        <h2>Nome: <span className="highlight">Lucas Almeida</span></h2>
+        <h2>Cpf: <span className="highlight">985.657.158-15</span></h2>
+        <h2>Email: <span className="highlight">Lucas.almeida@gmail.com</span></h2>
+        <h2>Celular: <span className="highlight">(85) 98575-2562</span></h2>
+        <hr className="divisor" />
+        </div>
 
-          </div>
+
+        <div className="delivery-info">
+        <h1>Informações de Entrega</h1>
+        <h2>Endereço: <span className="highlight">Av. Frei Cirilo, 269</span></h2>
+        <h2>Bairro: <span className="highlight">Messejana</span></h2>
+        <h2>Cidade: <span className="highlight">Fortaleza, Ceara</span></h2>
+        <h2>Cep: <span className="highlight">(85) 98578-698</span></h2>
+        </div>
         </section>
-
-      </div>
+            </div>
     </>
   );
 }
